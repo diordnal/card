197b21e1e30e5fee6255a6efea36e8ebe47e6a93
liu <812788714@qq.com>
2015-04-11 00:04:31 +0800

    Initial commit

---
+++ src/main/java/com/example/administrator/myapplication/main.java
@@ -0,0 +1,16 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/4/10.
+ */
+public class main extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
